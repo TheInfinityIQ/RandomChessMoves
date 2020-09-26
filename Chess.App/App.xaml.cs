5eea593ad540d7dfa44f6194732846a8de5b7822
Dawson Kelly <46154365+TheInfinityIQ@users.noreply.github.com>
2020-09-26 11:09:36 -0500

    Created Chess Project with WPF, .Net CORE Lib, XUnit

--- Chess.App/App.xaml.cs
+++ Chess.App/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace RandomChessMoves
+namespace Chess.App
 {
     /// <summary>
     /// Interaction logic for App.xaml
